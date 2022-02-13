f18ac18c43d9a08a9bbb65f2bbdd60f6097e4402
Juliananikole24 <98923773+Juliananikole24@users.noreply.github.com>
2022-02-13 18:21:40 -0500

    Add files via upload

---
+++ class.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 10 19:56:43 2022
+
+@author: Usuario
+"""
+
+class LaClaseMasSimple:
+    pass
+
+miprimerobjeto=LaClaseMasSimple()
---
+++ try.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 10 18:12:58 2022
+
+@author: Usuario
+"""
+try:
+    x=int(input("Enter a number: "))
+    y=1/x
+    print(y)
+except ZeroDivisionError:
+    print("You cannot divide by zero, sorry.")
+except ValueError:
+    print("You must enter an integer value.")
+except:
+    print("Oh dear, something went wrong...")
+    print("THE END")
+
+   
